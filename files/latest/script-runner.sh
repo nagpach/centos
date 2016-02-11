5f1f22162dc9b71d4b9f5b1acbae23ce01b5a162
Dennis Günnewig <dg1@ratiodata.de>
2016-02-11 14:56:41 +0100

    Fixed problem with less

--- files/latest/script-runner.sh
+++ files/latest/script-runner.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-script_directory=$1
+script_directory=$*
 
 if [ -z "$script_directory" ]; then
   echo "NO script directory given. Exiting..."
   exit 1
 fi
 
-for file in $(find "$script_directory" -type f | sort --human-numeric-sort); do
+for file in $(find $script_directory -type f | sort --human-numeric-sort); do
   if [ -x "$file" ]; then
     echo "Running command \"$file\"." >&2
     bash -c "$file"
